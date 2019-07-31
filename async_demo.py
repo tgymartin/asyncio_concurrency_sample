634ab85e2cf22b187c62da539f3afeb0ee792197
Tan Guan Yuan Martin <tgymartin@users.noreply.github.com>
2019-08-01 06:28:36 +0800

    Add files via upload

--- async_demo.py
+++ async_demo.py
@@ -2,6 +2,11 @@ import asyncio
 from concurrent.futures import ThreadPoolExecutor
 from time import sleep
 import sys
+import uvloop
+import socket
+
+HOST = 'localhost'
+PORT = 5000
 
 class Potato(object):
     def __init__(self):
@@ -10,8 +15,15 @@ class Potato(object):
         self._c = 1
         self._d = 0
         self._pool = ThreadPoolExecutor(max_workers=2)
+        uvloop.install()
         self._loop = asyncio.get_event_loop()
         self.coros = []
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.setblocking(False)
+        self.sock.bind((HOST, PORT))
+        self.sock.listen(10)
 
     def bogus(self, c):
         '''slow cpu bound task'''
@@ -44,24 +56,50 @@ class Potato(object):
             expected = self._a + self._b + self._c
             print('Result = {} , Expected = {}'.format(result, expected))
 
+    async def echo_handler(self, conn, addr):
+        while True:
+            msg = await self._loop.sock_recv(conn, 1024)
+            if not msg or msg is b'\xff\xf4\xff\xfd\x06':
+                break
+            # print(msg)
+            else:
+                try:
+                    msg_str = msg.decode().rstrip()[::-1]
+                except UnicodeDecodeError:
+                    break
+                print('Received: {} | From: {}'.format(msg_str, addr))
+                msg = (msg_str + '\n').encode()
+                await self._loop.sock_sendall(conn, msg)
+        print('Connection closed: {}'.format(addr))
+        conn.close()
+
+    async def echo_server(self):
+        while True:
+            conn, addr = await self._loop.sock_accept(self.sock)
+            print('Connection opened: {}'.format(addr))
+            self._loop.create_task(self.echo_handler(conn, addr))
+
     async def tasks(self):
         self.coros.append(self._loop.create_task(self.ship_to_process()))
         self.coros.append(self._loop.create_task(self.long_time()))
         self.coros.append(self._loop.create_task(self.hi()))
         self.coros.append(self._loop.create_task(self.sneaky()))
-        for coro in self.coros:
-            await asyncio.wait([coro])
+        self.coros.append(self._loop.create_task(self.echo_server()))
+        await asyncio.wait(self.coros)
         return self.coros
     
     def run(self):
         try:
-            self._loop.run_until_complete(self.tasks())
+            coros = self._loop.run_until_complete(self.tasks())
+            print(coros)
         except KeyboardInterrupt:
-            self._pool.shutdown(wait=False)
+            pass
+            # self._pool.shutdown(wait=False)
         else:
-            self._pool.shutdown(wait=True)
+            pass
         finally:
-            for task in asyncio.Task.all_tasks():
+            self._pool.shutdown(wait=True)
+            for task in asyncio.Task.all_tasks(self._loop):
                 task.cancel()
             self._loop.run_until_complete(self._loop.shutdown_asyncgens())
             self._loop.stop()
